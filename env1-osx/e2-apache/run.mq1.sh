ec7daa84140923515bbfcd3a4daa14ce327d1302
Pabloin <pablo.inchausti.bf@gmail.com>
2018-03-14 01:34:28 -0300

    Ejemplo e2

--- env1-osx/e2-apache/run.mq1.sh
+++ env1-osx/e2-apache/run.mq1.sh
@@ -5,5 +5,5 @@ export ELK_HOME=/Users/pabloin/Desktop/elk-stack/github/ELK-Stack-talk/env1-osx
 echo "(OSX Homebrew) Utilzia la version de logstash: logstash 6.2.2 "
 logstash --version
 
-logstash -f ${ELK_HOME}/e2-apache/logstash-h01.osx.conf --config.reload.automatic
+logstash -f ${ELK_HOME}/e2-apache/logstash_apache.conf --config.reload.automatic
 
